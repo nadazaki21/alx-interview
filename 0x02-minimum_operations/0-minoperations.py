4fecf32c2c47f0afa7a7ce0220c3d156829f654c
nadazkai21 <nadazaki019@gmail.com>
2024-08-11 12:57:30 +0300

    0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,14 +9,14 @@ of operations needed to result in exactly n H characters"""
 def minOperations(n):
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
-    
+
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-        
+
     return operations
