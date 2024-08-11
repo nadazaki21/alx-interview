96c706a88431856cae1b1abfdb33f67d335b0609
nadazkai21 <nadazaki019@gmail.com>
2024-08-11 12:48:30 +0300

    0

---
+++ 0x02-minimum_operations/0-main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+
+n = 10
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 3
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""" , there is a single character H. Your text editor can
+execute only two operations in this file: Copy All and Paste.
+Given a number n, write a method that calculates the fewestnumber
+of operations needed to result in exactly n H characters"""
+
+
+def minOperations(n):
+    """calculates the fewest number of operations needed
+    to result in exactly n H characters
+    """
+    operations = 2
+    divisor = 0
+    lmc = 0
+    for i in range(2, 10):
+        if n % i == 0:
+            divisor = i
+            if lmc < divisor:
+                lmc = divisor
+
+    print(f"lmc = {lmc}")
+    if lmc == 0:
+        return 0
+
+    intial_steps = lmc - 2
+    operations += intial_steps
+
+    if lmc != 0:
+        remaining_steps = n / lmc
+
+    operations = operations + remaining_steps
+
+    return operations - 1
